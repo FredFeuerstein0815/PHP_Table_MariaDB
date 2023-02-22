888bd98e4d9168a178ed706719cbed7fc90e9b63
FredFeuerstein0815 <110164290+FredFeuerstein0815@users.noreply.github.com>
2023-02-22 08:22:47 +0100

    Create bme280.php

---
+++ bme280.php
@@ -0,0 +1,48 @@
+<?php
+echo "<table style='border: solid 3px black; width:100%; text-align:center;'>";
+echo "<tr><th colspan='4'; style='border: solid 3px black; width:25%; text-align:center;'>BME280</th></tr>";
+echo "<tr><th style='border: solid 3px grey; width:25%; text-align:center;'>°C</th><th style='border: solid 3px grey; width:25%; text-align:center;'>%</th><th style='border: solid 3px grey; width:25%; text-align:center;'>hPa</th><th style='border: solid 3px grey; width:25%; text-align:center;'>Meßzeit</th></tr>";
+
+class TableRows1 extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='border: solid 3px black; width:25%; text-align:center;'>" . parent::current(). "</td>";
+  }
+
+  function beginbme280() {
+    echo "<tr style='border: solid 3px black; width:25%; text-align:center;'>";
+  }
+
+  function endbme280() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "localhost";
+$username = "sqlUsername";
+$password = "sqlPassword";
+$dbname = "datenbankname";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT temperatur, luftfeuchtigkeit, luftdruck, datumzeit FROM daten ORDER BY datumzeit DESC LIMIT 0,1");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+  foreach(new TableRows1(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+}
+catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+
+echo "</table>";
+?>
